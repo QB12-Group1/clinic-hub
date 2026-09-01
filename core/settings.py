4968a82a181e27a66c692bb88b5456ccf5d13ff5
Ilia Bahrebar <std.ilia.bahrebar@gmail.com>
2026-09-01 19:59:08 +0330

    chore(settings): register custom apps in INSTALLED_APPS

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Custom apps
+    "accounts.apps.AccountsConfig",
+    "appointments.apps.AppointmentsConfig",
+    "doctors.apps.DoctorsConfig",
+    "reviews.apps.ReviewsConfig",
+    "wallet.apps.WalletConfig",
 ]
 
 MIDDLEWARE = [
@@ -66,7 +72,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
