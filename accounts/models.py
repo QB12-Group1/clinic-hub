3c0912bf5771cb2a5791f4e4cc71dcb5a44927c3
Ilia Bahrebar <std.ilia.bahrebar@gmail.com>
2026-09-01 20:39:36 +0330

    feat(accounts)!: migrate to custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    class RoleChoices(models.TextChoices):
+        ADMIN = "admin", _("Admin")
+        PATIENT = "patient", _("Patient")
+        DOCTOR = "doctor", _("Doctor")
+
+    role = models.CharField(
+        max_length=10, choices=RoleChoices.choices, default=RoleChoices.PATIENT
+    )
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def is_admin(self) -> bool:
+        return self.role == self.RoleChoices.ADMIN
+
+    @property
+    def is_patient(self) -> bool:
+        return self.role == self.RoleChoices.PATIENT
+
+    @property
+    def is_doctor(self) -> bool:
+        return self.role == self.RoleChoices.DOCTOR
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
