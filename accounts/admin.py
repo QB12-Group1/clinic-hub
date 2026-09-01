6841a1bdcaa6bab95eebacd3af92a9e8082d2a7c
Bahar <baharmansourbeigi@gmail.com>
2026-09-02 02:11:03 +0330

    feat: register custom user in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    
+    list_display = ("username","role",)
+    list_filter = ("role",)
+    search_fields = ("username",)
+    ordering = ("-date_joined",)
+    
