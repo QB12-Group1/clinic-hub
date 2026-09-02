6a3adeb2c31f19f8a6a004df9625d7b4f22db04b
Bahar <baharmansourbeigi@gmail.com>
2026-09-02 16:16:11 +0330

    refactor: add type hint

--- doctors/models.py
+++ doctors/models.py
@@ -7,5 +7,5 @@ class Specialty(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
