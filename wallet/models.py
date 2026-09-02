7a33337320da1fa256d631f182ca74c813e3acd5
Bahar <baharmansourbeigi@gmail.com>
2026-09-02 23:37:08 +0330

    feat: add Transaction model

--- wallet/models.py
+++ wallet/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Wallet(models.Model):
@@ -8,3 +9,17 @@ class Wallet(models.Model):
 
     def __str__(self) -> str:
         return f"{self.user.username} ({self.balance})"
+
+
+class Transaction(models.Model):
+    class TransactionType(models.TextChoices):
+        CREDIT = "credit", _("Credit")
+        DEBIT = "debit", _("Debit")
+
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10, choices=TransactionType.choices)
+    amount = models.PositiveBigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.wallet} - {self.type} - {self.amount}"
