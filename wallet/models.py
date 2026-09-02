da4743eeed1d39d1cfdc4aae44fc51f92e0ece9c
Bahar <baharmansourbeigi@gmail.com>
2026-09-02 16:12:13 +0330

    add default

--- wallet/models.py
+++ wallet/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Wallet(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    balance = models.PositiveBigIntegerField()
+    balance = models.PositiveBigIntegerField(default=0)
 
     def __str__(self) -> str:
         return f"{self.user.username} ({self.balance})"
