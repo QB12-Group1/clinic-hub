e0a7a47105981da6bf6e18b9d1fa632c0bbb418c
Bahar <baharmansourbeigi@gmail.com>
2026-09-02 19:53:36 +0330

    feat: add Doctor model with user and specialty relationships

--- doctors/models.py
+++ doctors/models.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class Specialty(models.Model):
@@ -9,3 +11,17 @@ class Specialty(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Doctor(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    specialties = models.ManyToManyField(Specialty)
+    biography = models.TextField()
+    clinic_address = models.TextField()
+    clinic_phone = models.CharField(max_length=11)
+    visit_fee = models.PositiveBigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{_('Dr.')}{self.user.full_name}"
