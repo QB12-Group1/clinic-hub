db0672b04b4642b57152a6990f7792acb1ec931e
Bahar <baharmansourbeigi@gmail.com>
2026-09-02 16:34:40 +0330

    refactor: use get_user_model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+
+User = get_user_model()
+
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    
-    list_display = ("username","role",)
+    list_display = (
+        "username",
+        "role",
+    )
     list_filter = ("role",)
     search_fields = ("username",)
     ordering = ("-date_joined",)
-    
