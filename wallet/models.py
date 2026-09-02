05d14a3387cdd5415d571b2a46a5e6e0be58f4d0
Bahar <baharmansourbeigi@gmail.com>
2026-09-02 16:08:45 +0330

    refactor: change wallet balance to positive integer

--- wallet/models.py
+++ wallet/models.py
@@ -1,13 +1,10 @@
 from django.conf import settings
-from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Wallet(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    balance = models.DecimalField(
-        max_digits=12, decimal_places=2, default=0, validators=[MinValueValidator(0)]
-    )
+    balance = models.PositiveBigIntegerField()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.user.username} ({self.balance})"
