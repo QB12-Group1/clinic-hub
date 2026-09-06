538666efc5eecd453c9a10bc0097c0db66283a82
Bahar <baharmansourbeigi@gmail.com>
2026-09-06 17:31:05 +0330

    feat: add TimeSlot model and update wallet migration (#72)
    * feat: add TimeSlot model and update wallet migration
    
    * refactor(timeslot): simplify str method
    
    ---------
    
    Co-authored-by: Ilia Bahrebar <150489740+std-enigma@users.noreply.github.com>

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeSlot(models.Model):
+    doctor = models.ForeignKey(
+        "doctors.Doctor", related_name="time_slots", on_delete=models.CASCADE
+    )
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    is_booked = models.BooleanField(default=False)
+
+    class Meta:
+        constraints = (
+            models.CheckConstraint(
+                condition=models.Q(end_time__gt=models.F("start_time")),
+                name="timeslot_end_after_start",
+            ),
+        )
+
+        indexes = (
+            models.Index(
+                fields=["doctor", "is_booked", "start_time"],
+                name="timeslot_availablitiy_idx",
+            ),
+        )
+
+    def __str__(self) -> str:
+        status = "Booked" if self.is_booked else "Available"
+        return (
+            f"{self.doctor} -"
+            f"{self.start_time:%Y-%m-%d %H:%M} ({status})"
+        )
