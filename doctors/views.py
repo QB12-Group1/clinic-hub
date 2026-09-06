8ead8a1c27be754d66312ca3eb22b3fdbbd68c5c
Bahar <baharmansourbeigi@gmail.com>
2026-09-06 19:15:39 +0330

    feat: add doctor list

--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.db.models import QuerySet
+from django.views.generic.list import ListView
 
-# Create your views here.
+from doctors.models import Doctor
+
+
+class DoctorListView(ListView):
+    model = Doctor
+    template_name = ""
+    context_object_name = "doctors"
+
+    def get_queryset(self) -> QuerySet[Doctor]:
+        queryset = (
+            Doctor.objects.filter(account__is_active=True)
+            .select_related("account")
+            .prefetch_related("specialties")
+        )
+        return queryset
