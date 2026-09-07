e4881a8482e8ee3afd67644573ff8a7022780df8
Bahar <baharmansourbeigi@gmail.com>
2026-09-07 23:12:55 +0330

    Update doctors/views.py
    Co-authored-by: Ilia Bahrebar <150489740+std-enigma@users.noreply.github.com>

--- doctors/views.py
+++ doctors/views.py
@@ -7,7 +7,7 @@ from doctors.models import Doctor
 class DoctorListView(ListView):
     model = Doctor
     template_name = "pages/list.html"
-    context_object_name = "doctors"
+    context_object_name = "rows"
     paginate_by = 10
 
     def get_queryset(self) -> QuerySet[Doctor]:
