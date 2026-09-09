3f6031dd2d1b604a90bec0309a0897d43b1aaf01
Bahar <baharmansourbeigi@gmail.com>
2026-09-10 00:57:48 +0330

    feat: add review model

--- reviews/models.py
+++ reviews/models.py
@@ -2,13 +2,13 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
-class Review(models.Model): 
-    appointment = models.OneToOneField( "appointments.Appointment", related_name="review", on_delete=models.PROTECT ) 
-    comment = models.TextField(blank=True) 
-    rating = models.PositiveSmallIntegerField( validators=[MinValueValidator(1), MaxValueValidator(5)] ) 
-    created_at = models.DateTimeField(auto_now_add=True) 
+class Review(models.Model):
+    appointment = models.OneToOneField("appointments.Appointment", related_name="review", on_delete=models.PROTECT)
+    comment = models.TextField(blank=True)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         ordering = ["-created_at"]
         constraints = [
