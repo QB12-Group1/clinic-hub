6e2195c447394d2ed24a96d53b405a0b93c7cbea
Bahar <baharmansourbeigi@gmail.com>
2026-09-11 21:10:17 +0330

    feat: add wallet detail

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("doctors/", include("doctors.urls")),
+    path("wallet/", include("wallet.urls")),
 ]
