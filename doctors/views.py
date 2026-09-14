ff4377102fe89e2d1b417ace9bc7cfcddc2f3c53
Bahar <baharmansourbeigi@gmail.com>
2026-09-14 03:47:28 +0330

    feat: add doctor views

--- doctors/views.py
+++ doctors/views.py
@@ -2,7 +2,7 @@ from django.db.models import Q, QuerySet
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
-from accounts.mixins import StaffRequiredMixins
+from accounts.mixins import DoctorRequiredMixins, StaffRequiredMixins
 from doctors.models import Doctor, Specialty
 
 
@@ -37,27 +37,50 @@ class DoctorDetailView(DetailView):
         return Doctor.objects.filter(account__is_active=True).select_related("account").prefetch_related("specialties")
 
 
+class DoctorCreateView(StaffRequiredMixins, CreateView):
+    model = Doctor
+    fields = ["account", "specialties", "biography", "practice_address", "practice_phone_number", "visit_fee"]
+    template_name = "pages/create.html"
+    success_url = reverse_lazy("doctors:list")
+
+
+class DoctorUpdateView(DoctorRequiredMixins, UpdateView):
+    model = Doctor
+    fields = ["specialties", "biography", "practice_address", "practice_phone_number", "visit_fee"]
+    template_name = "pages/edit.html"
+    success_url = reverse_lazy("doctors:list")
+
+    def get_object(self, queryset=None):
+        return self.request.user.doctor_profile  # pyright: ignore[reportAttributeAccessIssue]
+
+
+class DoctorDeleteView(StaffRequiredMixins, DeleteView):
+    model = Doctor
+    template_name = "pages/delete.html"
+    success_url = reverse_lazy("doctors:list")
+
+
 class SpecialtyListView(ListView):
     model = Specialty
     template_name = "pages/list.html"
-    context_object_name = "Specialties"
+    context_object_name = "specialties"
 
 
 class SpecialtyCreateView(StaffRequiredMixins, CreateView):
     model = Specialty
-    template_name = "pages/create.html"
     fields = ["name", "description"]
+    template_name = "pages/create.html"
     success_url = reverse_lazy("doctors:specialty-list")
 
 
 class SpecialtyUpdateView(StaffRequiredMixins, UpdateView):
     model = Specialty
-    template_name = "pages/edit.html"
     fields = ["name", "description"]
+    template_name = "pages/edit.html"
     success_url = reverse_lazy("doctors:specialty-list")
 
 
 class SpecialtyDeleteView(StaffRequiredMixins, DeleteView):
     model = Specialty
-    template_name = "pages/Delete.html"
+    template_name = "pages/delete.html"
     success_url = reverse_lazy("doctors:specialty-list")
