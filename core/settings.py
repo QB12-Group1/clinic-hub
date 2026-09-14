41c9ae6e7e46aa85a4973f3628735c5c998a0c1a
Ilia Bahrebar <std.ilia.bahrebar@gmail.com>
2026-09-14 05:23:59 +0330

    fix: remove duplicate application entry

--- core/settings.py
+++ core/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "core",
     # Custom apps
     "core.apps.CoreConfig",
     "accounts.apps.AccountsConfig",
